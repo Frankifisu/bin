8c70effaf1d4a4661a330bfd014c3b0b99fa41a5
Franco Egidi <franco.egidi@gmail.com>
2019-10-02 12:45:36 +0200

    Polished fresul.py

--- fresul.py
+++ fresul.py
@@ -24,15 +24,19 @@ HOMEDIR = os.getenv('HOME')
 # ==========
 #  DEFAULTS
 # ==========
+# Permitted file extensions
 OUTEXT = frozenset(('.log', '.out'))
+# Patterns to search within file
 SRCXPR = {
     'Mode'   : None,
     'Freq'   : ' Frequencies -- ',
     'Sym'    : None,
     'IR'     : ' IR Inten    -- ',
     'Raman'  : ' Raman Activ -- ',
-    'redmas' : ' Red. masses -- '
+    'redmas' : ' Red. masses -- ',
+    'Start'  : ' Initial command:'
     }
+# Output strings for column headers
 OUTXPR = {
     'Mode'   : 'Mode',
     'Freq'   : 'Freq',
@@ -66,6 +70,12 @@ def parseopt():
     # MANDATORY ARGUMENTS
     parser.add_argument('outfil', help='Gaussian output file')
     # OPTIONAL ARGUMENTS
+    parser.add_argument('-t', '--table',
+        dest='tbf', action='store', default='simple',
+        help='Set table format')
+    parser.add_argument('-f', '--format',
+        dest='fmt', action='store', default='.2f',
+        help='Set float format')
     parser.add_argument('-v', '--iprint',
         dest='iprt', action='count', default=0,
         help='Set printing level')
@@ -85,32 +95,39 @@ def filparse(input_file) -> list:
     Parse file and return result list
     """
     # Array to collect all vibrational info
-    results = [[[]]]
+    idres = -1
+    results = []
     with open(input_file, 'r') as file_obj:
         # For Gaussian I have to go back two lines for symmetry and mode
         prevline = ['', '']
         modes = []
         for line in file_obj:
+            # Store one set of results for every calculation
+            if SRCXPR.get('Start') in line:
+                idres = idres + 1
+                results = padlist(results, idres, [])
+            if idres == -1:
+                continue
             # Get modes of interest and symmetry
             if SRCXPR.get('Freq') in line:
                 modes = [ int(number) for number in prevline[1].split() ]
                 syms = prevline[0].split()
-                idxmod, results[0][0] = listele(results[0][0], OUTXPR.get('Mode'))
-                idxsym, results[0][0] = listele(results[0][0], OUTXPR.get('Sym'))
+                results[idres] = padlist(results[idres], 0, [])
+                idxmod, results[idres][0] = listele(results[idres][0], OUTXPR.get('Mode'))
+                idxsym, results[idres][0] = listele(results[idres][0], OUTXPR.get('Sym'))
                 index = max(idxmod, idxsym)
                 for idat, vib in enumerate(modes):
-                    results[0] = padlist(results[0], vib, [])
-                    results[0][vib] = padlist(results[0][vib], index, None)
-                    results[0][vib][idxmod] = modes[idat]
-                    results[0][vib][idxsym] = syms[idat]
+                    results[idres] = padlist(results[idres], vib, [])
+                    results[idres][vib] = padlist(results[idres][vib], index, None)
+                    results[idres][vib][idxmod] = modes[idat]
+                    results[idres][vib][idxsym] = syms[idat]
             # Get info
-            results[0] = datasrc(line, modes, 'Freq', results[0])
-            results[0] = datasrc(line, modes, 'IR', results[0])
-            results[0] = datasrc(line, modes, 'Raman', results[0])
+            results[idres] = datasrc(line, modes, 'Freq',  results[idres])
+            results[idres] = datasrc(line, modes, 'IR',    results[idres])
+            results[idres] = datasrc(line, modes, 'Raman', results[idres])
             # Save line before overwriting in loop
             prevline[1] = prevline[0]
             prevline[0] = line
-    print(tabulate(results[0], headers='firstrow'))
     return results
 
 def datasrc(line: str, modes: list, toget: str, vibinfo: list) -> list:
@@ -119,6 +136,8 @@ def datasrc(line: str, modes: list, toget: str, vibinfo: list) -> list:
     listed in rows and extend vibinfo array
     """
     expr = SRCXPR.get(toget)
+    if expr is None:
+        return vibinfo
     if expr in line:
         datard = line.replace(expr, "").split()
         if len(modes) != len (datard):
@@ -160,6 +179,8 @@ def main():
     # PARSE OPTIONS
     opts = parseopt()
     results = filparse(opts.outfil)
+    for calc in results:
+        print(tabulate(calc, headers='firstrow', floatfmt=opts.fmt, tablefmt=opts.tbf))
     sys.exit()
 
 # ===========

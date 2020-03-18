585ca50b26cb17e2fd98dd81017a513a67ddec6a
Franco Egidi <franco.egidi@gmail.com>
2020-03-18 19:59:58 +0100

    Small changes to newinstall

--- newinstall.sh
+++ newinstall.sh
@@ -11,7 +11,7 @@ git config --global user.email "franco.egidi@gmail.com"
 git config --list
 sudo apt install openssh-server
 # ssh-keygen -t rsa
-mkdir -v -p ~/usr/bin && cd ~/usr/bin
+mkdir -v -p ~/usr/bin && cd ~/usr
 git clone https://github.com/Frankifisu/bin.git
 echo ' source $HOME/usr/bin/.muttrc' >> ~/.muttrc
 echo ' source $HOME/usr/bin/.vimrc' >> ~/.vimrc
